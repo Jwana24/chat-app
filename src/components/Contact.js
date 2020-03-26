92fe3a27a9d4e04ecff74a71257f1702e48277fc
Jwana24 <detrieux.btscom13@gmail.com>
2020-03-26 10:38:31 +0100

    create Contact component and css link in App.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,13 +3,16 @@ import "./Contact.css";
 
 function Contact()
 {
+    const online = true;
     return(
         <div className="Contact">
-            <div>
-                <img className="avatar" src="https://randomuser.me/api/portraits/men/17.jpg" />
-            </div>
+            <img className="avatar" src="https://randomuser.me/api/portraits/men/17.jpg" />
             <div>
                 <h6 className="name">Alex Mason</h6>
+                <div className="status">
+                    {online ? <p className="status-online"></p> : <p className="status-offline"></p>}
+                    {online ? <p className="status-text">online</p> : <p className="status-text">offline</p>}
+                </div>
             </div>
         </div>
     );
