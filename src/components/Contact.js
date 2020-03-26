43e249842bb920043378bf01bd584efc1afbc02c
Jwana24 <detrieux.btscom13@gmail.com>
2020-03-26 19:04:27 +0100

    updates app.js file with an array of objects ('user') and Contact.js component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,32 @@
+// Modules
 import React from 'react';
+import PropTypes from 'prop-types';
+// Components
 import "./Contact.css";
 
-function Contact()
+function Contact(user) // On passe le tableau user du parent à l'enfant
 {
-    const online = true;
-    return(
-        <div className="Contact">
-            <img className="avatar" src="https://randomuser.me/api/portraits/men/17.jpg" />
-            <div>
-                <h6 className="name">Alex Mason</h6>
-                <div className="status">
-                    {online ? <p className="status-online"></p> : <p className="status-offline"></p>}
-                    {online ? <p className="status-text">online</p> : <p className="status-text">offline</p>}
-                </div>
-            </div>
-        </div>
-    );
+   return(
+   // On va chercher dans le tableau user chaque objet indépendamment, selon notre besoin
+   <div className="Contact">
+      <img className="avatar" src={user.avatar} />
+      <div>
+         <h6 className="name">{user.name}</h6>
+         <div className="status">
+            {user.online ? <p className="status-online"></p> : <p className="status-offline"></p>}
+            {user.online ? <p className="status-text">online</p> : <p className="status-text">offline</p>}
+         </div>
+      </div>
+   </div>
+   );
 }
+
+/* Il s'agit du module PropTypes qui permet de vérifier et valider ou non les types passés dans les valeurs. Ici
+nous avons des types d'objets contenu dans "user" */
+Contact.propTypes = {
+   name: PropTypes.objectOf(PropTypes.user).isRequired,
+   avatar: PropTypes.objectOf(PropTypes.user).isRequired,
+   online: PropTypes.objectOf(PropTypes.user).isRequired,
+ };
 
 export default Contact;
